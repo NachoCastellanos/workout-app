2c26728728fec34d095ab9511d717dec568be0a9
Nacho Castellanos Moreno <nacho2casmor@gmail.com>
2024-10-08 09:43:27 +0200

    Second commit

--- src/main/java/com/castemore/workout_app_backend/controller/HelloWorldController.java
+++ src/main/java/com/castemore/workout_app_backend/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hola")
     public String sayHello() {
-        return "Hola Mundo";
+        return "Hola Mundo me llamo Nacho y este es el segundo commit";
     }
 }
